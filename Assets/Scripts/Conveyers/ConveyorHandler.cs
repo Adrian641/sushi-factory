31876f778800fecae20304e47d0e74749941c4ae
Adrian641 <adrianbaloi44@gmail.com>
2025-10-28 23:51:54 -0400

    Update ConveyorHandler.cs

--- Assets/Scripts/Conveyers/ConveyorHandler.cs
+++ Assets/Scripts/Conveyers/ConveyorHandler.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ConveyorHandler : MonoBehaviour
 {
@@ -9,6 +11,11 @@ public class ConveyorHandler : MonoBehaviour
     Transform conveyorBelts;
 
     int numberConveyorGroups = 0;
+    int currentConveyorGroupsLength = 0;
+    int allConveyorsNb = 0;
+    public Vector2Int[] start;
+    public Vector2Int[] end;
+
 
     void Start()
     {
@@ -21,48 +28,105 @@ public class ConveyorHandler : MonoBehaviour
         if (conveyors.isHandling)
         {
             numberConveyorGroups = conveyorBelts.childCount;
-            DecriptConveyorBelts(numberConveyorGroups);
+            for (int i = 0; i < numberConveyorGroups; i++)
+            {
+                Vector2Int[] beltPositions;
+                beltPositions = DecriptConveyorBelts(numberConveyorGroups, i);
+                Deconstruct(beltPositions, i);
+                allConveyorsNb += currentConveyorGroupsLength;
+                MergeBeltGroups(beltPositions, i, currentConveyorGroupsLength, allConveyorsNb);
+            }
+            Debug.Log(allConveyorsNb);
             conveyors.isHandling = false;
+            allConveyorsNb = 0;
         }
-
     }
 
-    public Vector2Int[] DecriptConveyorBelts(int nbOfConveyorGroups)
+    public Vector2Int[] DecriptConveyorBelts(int nbOfConveyorGroups, int i)
     {
         Vector2Int[] allActiveBelts = new Vector2Int[conveyors.arrayLimits];
         string BeltName;
         string component = "";
-        for (int i = 0; i < nbOfConveyorGroups; i++)
+        bool isFirstComponent = true;
+        Transform conveyorGroupX = conveyorBelts.transform.GetChild(i);
+        int conveyorGroupXNbChild = conveyorGroupX.childCount;
+
+        for (int j = 0; j < conveyorGroupXNbChild; j++)
         {
-            Transform conveyorGroupX = conveyorBelts.transform.GetChild(i);
-            int conveyorGroupXNbChild = conveyorGroupX.childCount;
-            for (int j = 0; j < conveyorGroupXNbChild; j++)
+            Transform BeltsTransform = conveyorGroupX.transform.GetChild(j);
+            GameObject BeltsGameObject = BeltsTransform.gameObject;
+            BeltName = BeltsGameObject.name;
+            for (int k = 0; k < BeltName.Length; k++)
             {
-                Transform BeltsTransform = conveyorGroupX.transform.GetChild(j);
-                GameObject BeltsGameObject = BeltsTransform.gameObject;
-                BeltName = BeltsGameObject.name;
-                for (int k = 0; k < BeltName.Length; k++)
+                if (BeltName[k] != ',' && k != BeltName.Length - 1)
                 {
-                    bool isFirstComponent = true;
-                    if (BeltName[k] != ',')
-                    {
-                        component += BeltName[k];
-                        Debug.Log(component);
-                    }
-                    else if (isFirstComponent)
-                    {
-                        allActiveBelts[j].x = int.Parse(component);
-                        component = "";
-                    }
-                    else
-                    {
-                        allActiveBelts[j].y = int.Parse(component);
-                        component = "";
-                    }
-                    Debug.Log(allActiveBelts[j]);
+                    component += BeltName[k];
+                }
+                else if (isFirstComponent)
+                {
+                    allActiveBelts[j].x = int.Parse(component);
+                    component = "";
+                    isFirstComponent = false;
+                }
+                else
+                {
+                    component += BeltName[k];
+                    allActiveBelts[j].y = int.Parse(component);
+                    component = "";
+                    isFirstComponent = true;
+                }
+            }
+            //Debug.Log(allActiveBelts[j]);
+        }
+        currentConveyorGroupsLength = conveyorGroupXNbChild;
+        return allActiveBelts;
+    }
+
+    public void Deconstruct(Vector2Int[] array, int counter)
+    {
+        Transform conveyorGroupX = conveyorBelts.transform.GetChild(counter);
+        int conveyorGroupXNbChild = conveyorGroupX.childCount;
+        GameObject NewConveyorGroupX;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i + 1] == Vector2Int.zero)
+                break;
+
+            if (array[i].x != array[i + 1].x && array[i].y != array[i + 1].y)
+            {
+                NewConveyorGroupX = new GameObject($"conveyorGroup{i}");
+                NewConveyorGroupX.transform.parent = this.transform;
+            }
+        }
+    }
+
+    public void MergeBeltGroups(Vector2Int[] array, int counter, int currentArrayLength, int arrayLength)
+    {
+        start = new Vector2Int[counter + 1];
+        end = new Vector2Int[counter + 1];
+        
+
+        if (counter > 0)
+        {
+            
+            for (int i = 0; i < start.Length - 1; i++)
+            {
+                if (array[arrayLength - currentArrayLength]  == start[i] || array[arrayLength - currentArrayLength] == end[i])
+                {
+                    //Debug.Log($"{end[i]}{start[i]}{array[arrayLength - currentArrayLength]}");
+                }
+                if (array[counter] == start[i] && array[counter] == end[i])
+                {
+                    //Debug.Log($"{array[counter]}");
                 }
             }
         }
-        return allActiveBelts;
+        else
+        {
+            start[counter] = array[0];
+            end[counter] = array[currentArrayLength - 1];
+            //Debug.Log($"{end[counter]}{start[counter]}{array[arrayLength - currentArrayLength]}");
+        }
     }
 }
