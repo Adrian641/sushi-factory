ba07d4dada7aa83ae10e2213c7a8d4b3d47750e9
dev-Devy <alexbaloi33@gmail.com>
2025-10-29 18:56:58 -0400

    volume settings functional
    volumje settings only in game
    main menu options converted to credits

--- Assets/Scripts/Menus/VolumeSettings.cs
+++ Assets/Scripts/Menus/VolumeSettings.cs
@@ -8,10 +8,39 @@ public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider SFXSlider;
+    [SerializeField] private Slider UISlider;
+
+    private void Start()
+    {
+        SetMusicVolume();
+        SetMasterVolume();
+        SetSFXVolume();
+        SetUIVolume();
+    }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
-        myMixer.SetFloat("music", volume);
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetMasterVolume()
+    {
+        float volume = masterSlider.value;
+        myMixer.SetFloat("master", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetUIVolume()
+    {
+        float volume = UISlider.value;
+        myMixer.SetFloat("UI", Mathf.Log10(volume) * 20);
     }
 }
