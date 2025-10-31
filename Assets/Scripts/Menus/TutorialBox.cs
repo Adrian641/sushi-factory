91db6a67a3a98e35bd30a0fbf40f124e42de65da
dev-Devy <alexbaloi33@gmail.com>
2025-10-31 01:08:54 -0400

    Proper tutorial that looks pretty good

---
+++ Assets/Scripts/Menus/TutorialBox.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TutorialBox : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+    
+    public GameObject bottomHighlight;
+    public GameObject questHighlight;
+    public GameObject messageHighlight;
+
+    private int index;
+
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialog();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("enter");
+            if (textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];
+            }
+        }
+
+        if (textComponent.text == lines[1])
+        {
+            bottomHighlight.SetActive(true);
+        }
+        else
+        {
+            bottomHighlight.SetActive(false);
+        }
+
+        if (textComponent.text == lines[2])
+        {
+            questHighlight.SetActive(true);
+        }
+        else
+        {
+            questHighlight.SetActive(false);
+        }
+
+        if (textComponent.text == lines[3])
+        {
+            messageHighlight.SetActive(true);
+        }
+        else
+        {
+            messageHighlight.SetActive(false);
+        }
+    }
+
+    void StartDialog()
+    {
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c  in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
