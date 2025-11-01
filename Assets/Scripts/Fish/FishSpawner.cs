328bc56e01ed6b64b059f29e373d3d87be78c0de
dev-Devy <alexbaloi33@gmail.com>
2025-10-31 20:57:28 -0400

    ui fixed

--- Assets/Scripts/Fish/FishSpawner.cs
+++ Assets/Scripts/Fish/FishSpawner.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class FishSpawner : MonoBehaviour
 {
     public float SpawningTime;
-    public GameObject ToSpawn1, ToSpawn2, ToSpawn3, ToSpawn4;
+    public GameObject ToSpawn1, ToSpawn2, ToSpawn3, ToSpawn4, ToSpawn5, ToSpawn6;
     public int pickObject;
 
     public GameObject mainCam;
 
+    public float camX;
+    public float camY;
 
     void Start()
     {
@@ -18,9 +20,10 @@ public class FishSpawner : MonoBehaviour
 
     void SpawnNow()
     {
+        camX = mainCam.transform.position.x;
+        camY = mainCam.transform.position.z;
+
         Quaternion rotation = Quaternion.Euler(90f, 0, 0);
-        float camX = mainCam.transform.position.x;
-        float camY = mainCam.transform.position.z;
 
         if (pickObject == 1)
         {
@@ -46,6 +49,18 @@ public class FishSpawner : MonoBehaviour
             float y = Random.Range(camY + 10f, camY - 10f);
             Instantiate(ToSpawn4, new Vector3(x, 4, y), rotation);
         }
+        if (pickObject == 5)
+        {
+            float x = Random.Range(camX + 10f, camX - 10f);
+            float y = Random.Range(camY + 10f, camY - 10f);
+            Instantiate(ToSpawn5, new Vector3(x, 4, y), rotation);
+        }
+        if (pickObject == 6)
+        {
+            float x = Random.Range(camX + 10f, camX - 10f);
+            float y = Random.Range(camY + 10f, camY - 10f);
+            Instantiate(ToSpawn6, new Vector3(x, 4, y), rotation);
+        }
     }
 
     void Update()
@@ -53,7 +68,7 @@ public class FishSpawner : MonoBehaviour
         SpawningTime -= Time.deltaTime;
         if (SpawningTime <= 0)
         {
-            pickObject = Random.Range(1, 5);
+            pickObject = Random.Range(1, 7);
             SpawnNow();
             SpawningTime = Random.Range(2.5f, 5.5f);
         }
---
+++ Assets/Scripts/Fish/MoveFish.cs
@@ -0,0 +1,30 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class MoveFish : MonoBehaviour
+//{
+//    private Vector3 startPos;
+//    private Vector3 endPos;
+//    private float fishDirection;
+
+//    public GameObject mainCam;
+//    public float camX;
+//    public float camY;
+    
+//    void Awake()
+//    {
+//        camX = mainCam.transform.position.x;
+//        camY = mainCam.transform.position.z;
+
+//        startPos = transform.position;
+//        endPos = new Vector3(camX, 4f, camY) * 2;
+
+//        fishDirection = startPos - endPos;
+//    }
+
+//    void Update()
+//    {
+
+//    }
+//}
